b3f7bb72570c7cefe27f454b2b2b8146e760bd70
Vladimir <sukycar@gmail.com>
2021-01-27 13:09:45 +0100

    Initial Commit

---
+++ grckiKino/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  grckiKino
+//
+//  Created by Vladimir Sukanica on 27.1.21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
